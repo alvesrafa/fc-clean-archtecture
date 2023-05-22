beb4ef032f64723c0e2e139d29cc3bd6e5967261
Rafael Alves <alvesrafa.dev@gmail.com>
2023-05-22 09:11:38 -0300

    feat: update usecase

---
+++ src/usecase/product/update/update.product.spec.ts
@@ -0,0 +1,49 @@
+import Product from "../../../domain/product/entity/product";
+import { UpdateProductUseCase } from "./update.product.usecase";
+
+const product = new Product("123", "Produto Top", 1000);
+
+const input = {
+  id: product.id,
+  name: "Updated Product",
+  price: 500,
+};
+
+const MockRepository = () => {
+  return {
+    create: jest.fn(),
+    findAll: jest.fn(),
+    find: jest.fn((id: string) =>
+      id === product.id ? Promise.resolve(product) : undefined
+    ),
+    update: jest.fn(),
+  };
+};
+
+describe("should update a product", () => {
+  it("should update a customer", async () => {
+    const productRepository = MockRepository();
+
+    const usecase = new UpdateProductUseCase(productRepository);
+
+    const output = await usecase.execute(input);
+
+    expect(output).toEqual(input);
+  });
+
+  it("should throw a error when id is invalid", async () => {
+    const productRepository = MockRepository();
+
+    const usecase = new UpdateProductUseCase(productRepository);
+
+    const inputWithWrongId = {
+      ...input,
+      id: "wrong",
+    };
+    console.log("inputWithWrongId", inputWithWrongId);
+
+    await expect(usecase.execute(inputWithWrongId)).rejects.toThrow(
+      "Product not found"
+    );
+  });
+});
---
+++ src/usecase/product/update/update.product.usecase.ts
@@ -0,0 +1,30 @@
+import ProductRepositoryInterface from "../../../domain/product/repository/product-repository.interface";
+import {
+  InputUpdateProductDto,
+  OutputUpdateProductDto,
+} from "./update.product.dto";
+
+export class UpdateProductUseCase {
+  productRepository: ProductRepositoryInterface;
+
+  constructor(repository: ProductRepositoryInterface) {
+    this.productRepository = repository;
+  }
+
+  async execute(input: InputUpdateProductDto): Promise<OutputUpdateProductDto> {
+    const product = await this.productRepository.find(input.id);
+    console.log("product", product);
+    if (!product) throw new Error("Product not found");
+
+    product.changeName(input.name);
+    product.changePrice(input.price);
+
+    await this.productRepository.update(product);
+
+    return {
+      id: product.id,
+      name: product.name,
+      price: product.price,
+    };
+  }
+}
