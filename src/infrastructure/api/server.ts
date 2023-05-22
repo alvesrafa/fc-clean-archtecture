5917b8f82836f4713914bfb2138500b6b70903f0
Rafael Alves <alvesrafa.dev@gmail.com>
2023-05-22 17:23:03 -0300

    test: api e2e

--- src/infrastructure/api/server.ts
+++ src/infrastructure/api/server.ts
@@ -1,8 +1,8 @@
-import { app } from "./express";
 import dotenv from "dotenv";
+import { app } from "./express";
 
 dotenv.config();
-const port: number = Number(process.env.PORT) || 3000;
+const port: number = Number(process.env.PORT) || 3333;
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
